a55c6dae803d05f4dfc87a264caf190435e0b1c4
yousefhassan1999 <yousefhassan121999@gmail.com>
2023-01-04 20:44:38 +0200

    Start BarChart

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import './App.css';
-import SideBar from './Components/SideBar/SideBar';
-import Header from './Components/Header/Header'
+import "./App.css";
+import SideBar from "./Components/SideBar/SideBar";
+import Header from "./Components/Header/Header";
+import Body from "./Components/Body/Body";
 const App = () => {
   return (
     <div className="Main">
       <SideBar />
-      <Header />
+      <div className="TOP-DownDiv">
+        <Header />
+        <Body />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Components/Body/Body.jsx
@@ -0,0 +1,11 @@
+import "./Body.css";
+import Dashboard from "./DashboarPage/Dashboard";
+const Body = () => {
+  return (
+    <div className="body">
+        <Dashboard />
+    </div>
+  );
+};
+
+export default Body;
